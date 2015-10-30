7cade1a0fbe6f413cd780e0d1dc57ae7065faeca
Rudy Hardeman (PD0ZRY) <pd0zry@gigafreak.net>
2015-10-30 22:48:13 +0100

    Added SE language tnx to sm0tsc (Johan)

---
+++ language/se.php
@@ -0,0 +1,30 @@
+<?php
+
+$language[''] = "";
+$language['DRTM'] = "DMR Real time Monitor";
+$language['possible'] = "Backend och mjukvara av";
+$language['home'] = "Hem";
+$language['news'] = "Nyheter";
+$language['map'] = "Karta";
+$language['how'] = "Hur fungerar det?";
+$language['legenda'] = "Teckenf&#246;rklaring";
+$language['forward_power'] = "PA Uteffekt";
+$language['reflected_power'] = "Reflekterad Uteffekt";
+$language['pa_temp'] = "PA Temperatur";
+$language['voltage'] = "Sp&#228;nning";
+$language['user'] = "Anv&#228;ndare";
+$language['last_time_qso'] = "Tid sen senaste QSO";
+$language['talkgroup'] = "Talgrupp";
+$language['legal'] = "Inga r&#228;ttigheter eller f&#246;rpliktelser";
+$language['contact'] = "Kontakt";
+$language['repeater'] = "Repeater";
+// used for "Repeater in City"
+$language['in'] = "i";
+$language['loading'] = "Laddar";
+$language['ago'] = "sedan";
+$language['years'] = "&#229;r";
+$language['months'] = "m&#229;nader";
+$language['days'] = "dagar";
+$language['hours'] = "timmar";
+$language['minutes'] = "minuter";
+
