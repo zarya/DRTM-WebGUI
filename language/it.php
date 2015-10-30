42b1fc7eb25d0599e29417a72fb3cc39175b7f93
Rudy Hardeman (PD0ZRY) <pd0zry@gigafreak.net>
2015-10-30 22:46:48 +0100

    Added IT language tnx to iw7dzr, Giacomo

---
+++ language/it.php
@@ -0,0 +1,32 @@
+<?php
+
+$language[''] = "";
+$language['DRTM'] = "DMR Real time Monitor";
+$language['possible'] = "Reso possibile da";
+$language['home'] = "Home";
+$language['news'] = "Notizie";
+$language['map'] = "Mappa";
+$language['how'] = "How";
+$language['legenda'] = "Legenda";
+$language['forward_power'] = "PA potenza";
+$language['reflected_power'] = "Potenza riflessa";
+$language['pa_temp'] = "PA Temperatura";
+$language['voltage'] = "Voltaggio";
+$language['user'] = "Utente";
+$language['last_time_qso'] = "Tempo dall'ultimo QSO";
+$language['talkgroup'] = "Talkgroup";
+$language['ts'] = "Timeslot";
+$language['legal'] = "Nulla in questo sito crea eventuali diritti o obblighi";
+$language['contact'] = "Contatti";
+$language['repeater'] = "Ripetitore";
+// used for "Repeater in City"
+$language['in'] = "in";
+$language['loading'] = "Caricamento";
+
+$language['ago'] = "fa";
+$language['years'] = "anno";
+$language['months'] = "mese";
+$language['days'] = "giorno";
+$language['hours'] = "ora";
+$language['minutes'] = "minuti";
+$language['seconds'] = "secondi";
